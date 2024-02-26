c2418c5800301a8fb12c0220dc2123a7b81dda64
Thilinaxxx <pererathilina2001@gmail.com>
2024-02-26 19:10:45 +0530

    Removed welcome banner

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -41,7 +41,7 @@ function Dashboard() {
           <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
 
             {/* Welcome banner */}
-            <WelcomeBanner />
+          {/* <WelcomeBanner /> */}
 
             {/* Dashboard actions */}
             <div className="sm:flex sm:justify-between sm:items-center mb-8">
